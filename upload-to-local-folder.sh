38abaf0b634e13d1255631118c1236ab44ed8d2f
Diego Municio <dmunicio@gmail.com>
2021-12-22 16:50:34 +0100

    prueba

--- upload-to-local-folder.sh
+++ upload-to-local-folder.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 BUCKET="devops-chicago-ta1-artifactory"
 VERSION=$(cat version.txt | head -n 1)
-cp -fp cuenta /opt/versions/cuenta-$VERSION
-echo "Uploaded to /opt/versions/cuenta-$VERSION"
+if [ ! -f /opt/versions/cuenta-$VERSION ]
+then
+    cp -fp cuenta /opt/versions/cuenta-$VERSION
+    echo "Uploaded to /opt/versions/cuenta-$VERSION"
+else
+    echo "/opt/versions/cuenta-$VERSION already uploaded"
+fi
